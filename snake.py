f5ee7737b4602f060e2bed20ab986cc4e120b351
brunodos-santos <bruno6.santos@usp.br>
2023-07-26 12:52:56 -0300

    Inclusão do da maior pontuação feita no jogo

--- snake.py
+++ snake.py
@@ -7,7 +7,7 @@ LEFT = 180
 RIGHT = 0
 
 
-class Snake:
+class Snake():
 
     def __init__(self):
         self.segments = []
@@ -27,6 +27,13 @@ class Snake:
 
     def extend(self):
         self.add_segment(self.segments[-1].position())
+
+    def reset(self):
+        for seg in self.segments:
+            seg.goto(1000, 1000)
+        self.segments.clear()
+        self.create_snake()
+        self.head = self.segments[0]
 
     def move(self):
         for seg_num in range(len(self.segments) - 1, 0, -1):
